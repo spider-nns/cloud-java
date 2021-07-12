2e57b87f5cb2239c40819134b94047aa84a7fdf0
spider-nns <spider_nns@126.com>
2021-07-12 23:55:42 +0800

    init cloud_eureka_demo

---
+++ consumer/src/main/java/dev/spider/consumer/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package dev.spider.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}
